3d7295911ff4384b0637752704f68e6998975a38
RahulSharma <rahulmlc2003@gmail.com>
2023-11-22 17:38:13 +0530

    this is task 2 in which we added meal component(Meals, MealsSummary, AvailableMeals)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import Header from "./components/Layout/Header";
+import Meals from './components/Meal/Meals'
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
